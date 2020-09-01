b24e3d14fff7de5f782c98e2769853651c424a0c
Elisha Kramer <kramer1346@gmail.com>
2020-09-01 14:21:40 +0300

    file listAllTags succesfully adds contact and them adds the relevant tag

--- listAllTags.js
+++ listAllTags.js
@@ -30,6 +30,28 @@ async function allTags() {
 
 allTags();	
 
+let email = 'alephmarketingpros@gmail.com';
+var contact_add = ac.api("contact/add", {email});
+
+contact_add.then(function(result) {
+	console.log('succesfully added contact',result);
+
+	var eventdata = {
+	    tags: 'installed-social-king',
+	    email
+	};
+
+	ac.api('contact/tag/add', eventdata).then(function(result) {
+	    console.log('success', result);
+	}, function(err) {
+	    console.log('failure', err);
+	});     
+}, function(result) {
+	console.log(result);
+});
+
+
+
 
 
 
